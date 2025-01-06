a6a1cfb4112b3ea294fa98af70eaf88ff9f4fb63
tracer-ics2019 <tracer@njuics.org>
2025-01-06 16:56:12 +0800

    >  compile U202115386 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:56:12 up  8:29,  1 user,  load average: 0.71, 0.45, 0.57 f1161cf71760dbb642cf7dbf25d516463344b698

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -7,17 +7,17 @@ void ui_mainloop(int);
 uint32_t expr(char *e, bool *success);
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
-  //int is_batch_mode = init_monitor(argc, argv);
+  int is_batch_mode = init_monitor(argc, argv);
 
   /* Receive commands from user. */
   //ui_mainloop(is_batch_mode);
-  int ans , result = 0;
+  u_int32_t ans , result = 0;
   char str[65536];
   bool success = true;
   FILE *fp = fopen("tools/gen-expr/input","r");
-  while(fscanf(fp,"%d %s",&ans,str) == 2)
+  while(fscanf(fp,"%d %s",&ans,str) != 1 && fscanf(fp, "%[^\n]",str) != 1)
   {
-    printf("ans = %d str = %s\n",ans,str);
+    printf("ans = %u str = %s\n",ans,str);
     result = expr(str,&success);
     if(success == false)
     {
