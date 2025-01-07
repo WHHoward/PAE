2c614f75a7d450264cb40121d08e426f66c6cb40
tracer-ics2019 <tracer@njuics.org>
2025-01-07 12:21:02 +0800

    >  compile U202115386 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:21:02 up 10:42,  1 user,  load average: 0.31, 0.45, 0.46 7f489107c4a4a5e766675ca663e9eec9a870a

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -61,6 +61,11 @@ void cpu_exec(uint64_t n) {
   log_clearbuf();
 
     /* TODO: check watchpoints here. */
+    if(check_wp() == false)
+    {
+      nemu_state.state = NEMU_STOP;
+      return ;
+    }
 
 #endif
 
